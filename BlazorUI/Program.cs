ae206092f97eac63a9868bcc5b380e74df800a17
ritikagerdes <ritikagerdes@gmail.com>
2025-11-07 13:46:28 -0600

    Update Program.cs

--- BlazorUI/Program.cs
+++ BlazorUI/Program.cs
@@ -6,6 +6,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// Configure HttpClient to use the API URL
+string apiUrl = builder.Configuration["ApiUrl"] ?? "http://localhost:5000";
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiUrl) });
 
 await builder.Build().RunAsync();
